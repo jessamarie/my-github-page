e10a2deb6a223d28503f1548e8e7255b72a3fe5f
jessamarie <Jessa>
2017-03-14 00:51:44 -0400

    modified fonts.js

--- gulp_tasks/fonts.js
+++ gulp_tasks/fonts.js
@@ -1,6 +1,8 @@
 const gulp = require('gulp');
+const conf = require('../conf/gulp.conf');
 
 gulp.task('fonts', fonts);
+
 function fonts(){
     return gulp.src([
         'bower_components/font-awesome/fonts/*',
